32071c8832f58f7f01a1384a1f4dfc9589bf948e
Thomas Lavocat <tlavocat@redhat.com>
2022-06-09 09:22:52 +0200

    test: test the manifest generation on the CI
    The script osbuild-image-test can dynamically filter based on the
    machine characteristics. We use an intermediary program to extracts the
    distribution name from /etc/os-release and the arch from os.platform.
    This way we can run the CI on each kind of machines/distros.

--- schutzbot/deploy.sh
+++ schutzbot/deploy.sh
@@ -3,7 +3,17 @@ set -euxo pipefail
 
 
 # set locale to en_US.UTF-8
-sudo dnf install -y glibc-langpack-en git
+sudo dnf install -y glibc-langpack-en git make
+sudo dnf -y install skopeo \
+        make \
+        git \
+        go \
+        openssl \
+        rpm-build \
+        setools-console \
+        setroubleshoot \
+        container-selinux \
+        lvm2
 localectl set-locale LANG=en_US.UTF-8
 
 # Colorful output.
@@ -18,6 +28,7 @@ git clone https://github.com/osbuild/osbuild.git
 # install the latest osbuild
 
 cd osbuild
+sudo dnf -q builddep -y osbuild.spec
 make rpm
 sudo dnf install ./rpmbuild/RPMS/noarch/*.rpm  -y
 

5fffbf33026c556f0c461614f3a6b7e39ab12668
Thomas Lavocat <tlavocat@redhat.com>
2022-11-08 16:41:33 +0100

    ci: add a second behavior for the trigger pipeline
    By default the trigger pipeline will perform the update scenario where
    it will build the manifest from composer's main branch and generate the
    image info in order to create a pull request.
    
    A second behavior is provided by the special trigger variable TRIGGER_KIND
    that will:
    - generate the manifests from composer's main
    - generate the manifests from $COMPOSER_REMOTE's $ COMPOSER_BRANCH
    - only keep the ones that changed from the remote
    - build the images
    - test the image-info regarding the DB
    
    To use it:
    
    curl --request POST\
        --form token=$YOUR_TOKEN\
        --form ref=main\
        --form "variables[COMPOSER_REMOTE]=$SOME_REMOTE" \
        --form "variables[COMPOSER_BRANCH]=$SOME_BRANCH" \
        --form "variables[TRIGGER_KIND]=test" \
        https://gitlab.com/api/v4/projects/36844106/trigger/pipeline

--- schutzbot/gen_manifests.sh
+++ schutzbot/gen_manifests.sh
@@ -9,3 +9,23 @@ sudo dnf config-manager --set-enabled codeready-builder-for-rhel-9-rhui-rpms
 sudo dnf build-dep -y osbuild-composer.spec
 echo "Generating manifests"
 go run ./cmd/gen-manifests -workers 50 -output ../manifests
+
+if [ "${COMPOSER_REMOTE:-}" ]; then
+    cd ..
+    mv manifests tmp
+    mkdir manifests
+    git clone ${COMPOSER_REMOTE} osbuild-composer2
+    cd osbuild-composer2
+    git checkout ${COMPOSER_BRANCH}
+    echo "Installing build dependencies"
+    sudo dnf build-dep -y osbuild-composer.spec
+    echo "Generating manifests"
+    go run ./cmd/gen-manifests -workers 50 -output ../manifests2
+    cd ..
+    echo "Keeping only the different ones"
+    cd manifests2
+    for f in *
+    do
+         diff ${f} ../tmp/${f} || mv ${f} ../manifests/${f}
+    done
+fi
